5930913a2281a2bac0887c71d5513abaddeeb046
AMT98 <aswinmalla12@gmail.com>
2023-01-29 13:16:12 -0700

    Starting point

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,9 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="App w-full h-full flex flex-col items-center">
+      <div className='uppercase text-3xl mx-4 z-1 text-center md:mx-8 md:text-5xl'>Todo App</div>
     </div>
   );
 }
